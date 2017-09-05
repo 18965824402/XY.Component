a0455526eff48bf28f6c7a51b762e99fe4a29a3f
iQing <bdq123@outlook.com>
2017-09-05 16:29:16 +0800

    *

---
+++ XY.Component.DES/TripleDES.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace XY.Component.DES
+{
+    /// <summary>
+    /// 三重DES加密 解密 一旦确定不允许修改 否则之前加密的数据 无法解密
+    /// </summary>
+    public class TripleDES
+    {
+        const string key = "xingyuan2017auto";
+
+        /// <summary>
+        /// 加密
+        /// </summary>
+        /// <param name="source">明文</param>
+        /// <returns>返回密文</returns>
+        public static string Encrypt(string source)
+        {
+            TripleDESCryptoServiceProvider desCryptoProvider = new TripleDESCryptoServiceProvider();
+            MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider();
+
+            byte[] byteHash;
+            byte[] byteBuff;
+
+            byteHash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(key));
+            desCryptoProvider.Key = byteHash;
+            desCryptoProvider.Mode = CipherMode.ECB; //CBC, CFB
+            byteBuff = Encoding.UTF8.GetBytes(source);
+
+            string encoded =
+                Convert.ToBase64String(desCryptoProvider.CreateEncryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+            return encoded;
+        }
+
+        /// <summary>
+        /// 解密
+        /// </summary>
+        /// <param name="encodedText">密文</param>
+        /// <returns>返回明文</returns>
+        public static string Decrypt(string encodedText)
+        {
+            TripleDESCryptoServiceProvider desCryptoProvider = new TripleDESCryptoServiceProvider();
+            MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider();
+
+            byte[] byteHash;
+            byte[] byteBuff;
+
+            byteHash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(key));
+            desCryptoProvider.Key = byteHash;
+            desCryptoProvider.Mode = CipherMode.ECB; //CBC, CFB
+            byteBuff = Convert.FromBase64String(encodedText);
+
+            string plaintext = Encoding.UTF8.GetString(desCryptoProvider.CreateDecryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+            return plaintext;
+        }
+    }
+}
